53c319e3b2c26703d34edf8be621846ca02f2647
Alexy <aoskiev@gmail.com>
2023-02-18 13:34:20 +0100

    allure tests

---
+++ src/test/java/org/prog/AllureReportTests.java
@@ -0,0 +1,47 @@
+package org.prog;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Step;
+import org.junit.jupiter.api.Test;
+
+public class AllureReportTests {
+
+  @Test
+  @Description("this is a base tests")
+  @Severity(SeverityLevel.CRITICAL)
+  public void allureCritTest() {
+    printThing("This is a critical test");
+    printThing("It tests critical things");
+  }
+
+  @Test
+  @Description("this is a base tests")
+  @Severity(SeverityLevel.BLOCKER)
+  public void allureBlockTest() {
+    printThing("This is a blocker test");
+    printThing("It tests blocker things");
+  }
+
+  @Test
+  @Description("this is a base tests")
+  @Severity(SeverityLevel.MINOR)
+  public void allureMinorTestOne() {
+    printThing("This is a minor test");
+    printThing("It tests minor things");
+  }
+
+  @Test
+  @Description("this is a base tests")
+  @Severity(SeverityLevel.MINOR)
+  public void allureMinorTestTwo() {
+    printThing("This is a minor test");
+    printThing("It tests minor things");
+  }
+
+  @Step("I print things")
+  private void printThing(String s) {
+    System.out.println(s);
+  }
+}
